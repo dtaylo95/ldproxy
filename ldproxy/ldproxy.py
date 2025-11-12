0f2dbdff0543905c3e6dbc8724a9af6017604f38
Dylan Taylor <dtaylo95@jhu.edu>
2025-11-12 17:50:52 -0500

    Add testing data

--- ldproxy/ldproxy.py
+++ ldproxy/ldproxy.py
@@ -1,1 +1,32 @@
+#!/usr/bin/env python3
 
+"""
+(C) 2025 Dylan Taylor
+
+ldproxy is a command-line tool for generating privacy-preserving pseudo-
+genotypes that faithfully reproduce real-data LD structure.
+
+This allows sharing of LD information for applications like fine-mapping and
+colocalization without exposing raw genotype data.
+"""
+
+from argparse import ArgumentParser
+from logging import Logger
+
+
+def main():
+
+    # Parse command-line arguments
+    parser = ArgumentParser(description="ldproxy: Privacy-preserving LD proxy genotype generator")
+    
+    input_group = parser.add_argument_group(title="Input Options", description="Specify the input genotype file")
+    mut_input_group = input_group.add_mutually_exclusive_group(required=True)
+    mut_input_group.add_argument('--vcf', type=str, help="Input VCF file")
+    mut_input_group.add_argument('--bfile', type=str, help="Input PLINK binary file prefix")
+    mut_input_group.add_argument('--pgen', type=str, help="Input PLINK PGEN file prefix")
+
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
